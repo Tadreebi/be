341024f31d145f600fa2f7db80abc4897082408c
emad-almajdalawi <majdalawi97@yahoo.com>
2022-06-23 19:44:15 +0300

    Feat! : Custom User

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import AppUser
 
-# Register your models here.
+
+@admin.register(AppUser)
+class AdminAppUser(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "email",
+        "is_admin",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+    list_filter = ("is_admin", "is_active", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+    ordering = ("username",)
