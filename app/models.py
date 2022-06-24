5b25388b1953392e4b96d2724c0d10f661520925
makkahwi <suhaibahmadai@hotmail.com>
2022-06-24 08:21:16 +0300

    StudentReports Model Update

--- app/models.py
+++ app/models.py
@@ -6,15 +6,14 @@ choices = ["Weekly", "Monthly", "Final"]
 
 class StudentReports(models.Model):
     title = models.CharField(max_length=256, null=True, blank=True)
-    student = models.ForeignKey(
-        get_user_model(), on_delete=models.CASCADE, null=False, blank=False
-    )
+    student = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     type = models.CharField(choices=choices)
     # Start & end of period covered by the report
     startDate = models.DateField()
     endDate = models.DateField()
     # Report intro & conclusion paragraphs
-    intro = models.TextField()
+    intro = models.TextField(null=True, blank=True)
+    conclusion = models.TextField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
