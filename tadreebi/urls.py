4729c26d417850af3fe4fc01e28e63ef675b7aae
makkahwi <suhaibahmadai@hotmail.com>
2022-06-24 11:18:56 +0300

    StudentProfile URLs

--- tadreebi/urls.py
+++ tadreebi/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("students/", include("app.urls.studentReports")),
+    path("profile/", include("app.urls.studentProfile")),
 ]
