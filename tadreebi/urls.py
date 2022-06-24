e38b6203819a52820a7c9816f47299ea4eb0d593
makkahwi <suhaibahmadai@hotmail.com>
2022-06-24 10:58:30 +0300

    Minor Fix

--- tadreebi/urls.py
+++ tadreebi/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("students/", include("app.urls.StudentReports")),
+    path("students/", include("app.urls.studentReports")),
 ]
