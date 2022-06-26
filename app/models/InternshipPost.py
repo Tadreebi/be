68d17d74f3b5e792ba304554ff130d0d4c97a2fa
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-06-26 18:06:57 +0300

    Modifications on the posting model

--- app/models/InternshipPost.py
+++ app/models/InternshipPost.py
@@ -3,8 +3,8 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 
 class InternshipType(models.TextChoices):
-    FullTime = 'Full Time'
-    PartTime = 'Part Time'
+    Full_Time = 'Full Time'
+    Part_Time = 'Part Time'
     Remote = 'Remote'
 
 class Education(models.TextChoices):
@@ -13,10 +13,10 @@ class Education(models.TextChoices):
     Phd = 'Phd'
 
 class Experience(models.TextChoices):
-    NoExperience = 'No Experience'
-    OneYear = 'One Year'
-    TwoYears = 'Two Years'
-    ThreeYearsAndAbove = 'Three Years and Above'
+    No_Experience = 'No Experience'
+    One_Year = 'One Year'
+    Two_Years = 'Two Years'
+    Three_Years_And_Above = 'Three Years and Above'
 
 class Industry(models.TextChoices):
     Business = 'Business'
@@ -32,15 +32,16 @@ class InternshipSalary(models.TextChoices):
     UnPaid = 'UnPaid Internship'
 
 class PostInternship(models.Model):
-    company_name = models.CharField(max_length=150)
+    company_name = models.CharField(max_length=150, null=True)
     title = models.CharField(max_length=255, null=True)
     position = models.TextField()
-    description = models.TextField(null=True)
+    description = models.TextField(max_length=500, null=True)
+    min_requirements = models.TextField(max_length=500, null=True)
     email = models.EmailField(null=True)
     schedule = models.CharField(
         max_length=50,
         choices=InternshipType.choices,
-        default=InternshipType.FullTime
+        default=InternshipType.Full_Time
     )
     education = models.CharField(
         max_length=50,
@@ -55,17 +56,18 @@ class PostInternship(models.Model):
     experience= models.CharField(
         max_length=50,
         choices=Experience.choices,
-        default=Experience.NoExperience
+        default=Experience.No_Experience
     )
     internshipType= models.CharField(
         max_length=50,
         choices=InternshipSalary.choices,
         default=InternshipSalary.UnPaid
     )
+
     salary = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(10000000)])
     location = models.TextField(max_length=255, null=True)
     positions = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
-    last_date = models.DateTimeField(auto_now_add=False)
+    last_date = models.DateTimeField(auto_now_add=False, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
