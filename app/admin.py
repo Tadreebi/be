b4be8aa45e24d260da5b9a80f661b23c59ce3e6a
GhaidaMomani <ghaidaalmomani3@gmail.com>
2022-06-26 18:39:57 +0300

    initReports

--- app/admin.py
+++ app/admin.py
@@ -4,9 +4,11 @@ from app.models import (
     StudentReportSkill,
     StudentReportAchievement,
     StudentProfile,
+    report
 )
 
 admin.site.register(StudentReport)
 admin.site.register(StudentReportSkill)
 admin.site.register(StudentReportAchievement)
 admin.site.register(StudentProfile)
+admin.site.register(report.Report)
