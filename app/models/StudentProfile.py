989821dd1c085288f988352383abf171d5aab12f
makkahwi <suhaibahmadai@hotmail.com>
2022-06-27 21:58:44 +0300

    StudentUniProposal Model Fixes

--- app/models/StudentProfile.py
+++ app/models/StudentProfile.py
@@ -17,7 +17,9 @@ contacts = {
 
 class StudentProfile(models.Model):
     full_name = models.CharField(max_length=256, null=True, blank=True)
-    student = models.ForeignKey(StudentUser, on_delete=models.CASCADE, unique=True)
+    student = models.ForeignKey(
+        StudentUser, on_delete=models.CASCADE
+    )  # Switch one-to-one
     intro = models.TextField(null=True, blank=True)
     image = models.CharField(max_length=256, null=True, blank=True)
     job_title = models.CharField(max_length=256, null=True, blank=True)
--- app/models/StudentProposal.py
+++ app/models/StudentProposal.py
@@ -1,31 +1,26 @@
-from django.contrib.auth import get_user_model
+from pickle import FALSE
+from .Users import StudentUser, CompanyUser, UniversityEmployeeUser
 from django.db import models
 
 
 class StudentUniProposal(models.Model):
-    full_name = models.CharField(max_length=256)
-    student = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, unique=True)
-    student_Number= models.CharField(max_length=20)
-    company_Name=models.CharField(max_length=64)
-    company_adress=models.CharField(max_length=256)
-    company_phone_number=models.CharField(max_length=15)
-    supervisor_Name=models.CharField(max_length=256)
-    supervisor_phone_number=models.CharField(max_length=256)
-    subervisor_position=models.CharField(max_length=256)
-    submission_date=models.DateField()
+    student = models.ForeignKey(
+        StudentUser, on_delete=models.CASCADE, related_name="student_id", default=1
+    )
+    company = models.ForeignKey(
+        CompanyUser, on_delete=models.CASCADE, related_name="company_id", default=1
+    )  # DOuble check of default neccssaty
+    # Move supervisor data to opeertunity post data ##################
+    supervisor_Name = models.CharField(max_length=256)
+    supervisor_phone_number = models.CharField(max_length=256)
+    subervisor_position = models.CharField(max_length=256)
+    # ForeignKey to student application to oppertunity ##################
+    # Supervisor notes
+    remarks = models.TextField(null=True, blank=True)
+    accepted = models.BooleanField(default=False)
+    # Timestamps
     timestamp = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True)
 
-    
-
-
     def __str__(self):
         return self.company_Name
-
-
-
-
-
-
-
-    
