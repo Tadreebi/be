30e14960fe1f097888cb10d80017bf6a42b5917b
makkahwi <suhaibahmadai@hotmail.com>
2022-06-27 22:15:33 +0300

    StudentExperience Model Update

--- app/models/StudentExperience.py
+++ app/models/StudentExperience.py
@@ -1,14 +1,20 @@
 from time import time
-from django.contrib.auth import get_user_model
 from django.db import models
+from .Users import StudentUser, CompanyUser
+
 
 class StudentExperience(models.Model):
-    student = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    Company = models.CharField(max_length=64)
-    time = models.DateTimeField(auto_now_add=True)
-    recommend = models.BooleanField(null=True,blank=True)
-    detail_experience= models.TextField(max_length=256,null=True,blank=True)
-    timestamp = models.DateTimeField(auto_now_add=True,null=True)
+    student = models.ForeignKey(
+        StudentUser, on_delete=models.CASCADE, related_name="student_id"
+    )
+    company = models.ForeignKey(
+        CompanyUser, on_delete=models.CASCADE, related_name="company_id"
+    )
+    # ForeignKey to student application to oppertunity ##################
+    improved_aspects = models.TextField(max_length=5000, null=True, blank=True)
+    missed_aspects = models.TextField(max_length=5000, null=True, blank=True)
+    get_hired = models.TextField(max_length=5000, null=True, blank=True)
+    more = models.TextField(max_length=5000, null=True, blank=True)
+    # Timestamps
+    timestamp = models.DateTimeField(auto_now_add=True, null=True)
     updated = models.DateTimeField(auto_now=True)
-
-
