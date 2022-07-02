d2663e96ff897c8d48f52c252c91d366d0e4f60b
makkahwi <suhaibahmadai@hotmail.com>
2022-07-02 21:37:04 +0300

    StudentReport Split Model Reg

--- app/admin.py
+++ app/admin.py
@@ -18,6 +18,7 @@ from app.models import (
     StudentProfileLanguage,
     StudentProfileSkill,
     StudentProfileWork,
+    StudentReportType,
     StudentReport,
     StudentReportRemark,
     StudentReportSkill,
