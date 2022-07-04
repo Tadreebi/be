415be4a7472db1703ed4e23b833f7da7513eb7b5
makkahwi <suhaibahmadai@hotmail.com>
2022-07-05 02:09:23 +0300

    Remove remarks key of StudentUniProposal

--- app/models/StudentProposal.py
+++ app/models/StudentProposal.py
@@ -26,7 +26,6 @@ class StudentUniProposal(models.Model):
     # Uni Supervisor notes
 
     # Timestamps
-    remarks = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
 
